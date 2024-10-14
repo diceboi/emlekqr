85c613aa994b0976b51fda4e80a4cc6c742182a6
diceboi <szasz.szabolcs1995@gmail.com>
2024-10-14 15:30:46 +0200

    tbeditfill

--- app/components/Profil/ProfilEmlekadatlapok.jsx
+++ app/components/Profil/ProfilEmlekadatlapok.jsx
@@ -1,11 +1,12 @@
 import Emlekadatlaptile from "../UI/EmlekadatlapTile";
+import { TbEdit, TbBellRinging2Filled } from "react-icons/tb";
 
 export default function ProfilEmlekadatlapok({ session }) {
   return (
     <div className="flex flex-col gap-16 bg-white shadow-special rounded-2xl p-8">
         <div className="flex flex-col gap-8">
         <div className="flex flex-nowrap gap-4 items-center">
-            <TbBellRingingFilled className="w-8 h-8 text-[--rose] bg-[--cream] rounded-full p-1" />
+            <TbBellRinging2Filled className="w-8 h-8 text-[--rose] bg-[--cream] rounded-full p-1" />
             <h4>Emlékadatlapok</h4>
         </div>
         
