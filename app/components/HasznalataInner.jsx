e9fb47bbf26ddb1ee7eeaaec7a2389fb0843664d
diceboi <szasz.szabolcs1995@gmail.com>
2024-11-09 14:39:30 +0100

    hasznalata

--- app/components/HasznalataInner.jsx
+++ app/components/HasznalataInner.jsx
@@ -1,5 +1,6 @@
 import Paragraph from "../components/UI/Paragraph"
 import { TbInfoCircleFilled, TbHammer, TbCircleNumber1Filled, TbCircleNumber2Filled, TbCircleNumber3Filled  } from "react-icons/tb";
+import { CgDanger } from "react-icons/cg";
 import H1 from "./UI/H1";
 import H2 from "./UI/H2";
 import H3 from "./UI/H3";
@@ -53,11 +54,51 @@ export default function HasznalataInner() {
       </div>
 
     </div>
-    <div className="flex flex-col lg:py-16 py-8  lg:text-left text-center">
-      <H2 classname={"text-[--rose]"}>Az érme és használata</H2>
-      <div>
+    <div className="flex flex-col lg:py-16 py-8 lg:text-left text-center">
+      <H2 classname={"text-[--rose] lg:text-left text-center"}>Az érme és használata</H2>
+      <div className="flex flex-col py-16">
+
+      <div className="flex flex-col lg:gap-16 gap-8 py-8">
+
+        <div className="relative flex flex-col gap-4 w-full bg-[--cream] p-8 rounded-3xl">
+          <H3 classname={"text-[--rose]"}>
+            Az érme elhelyezése és tartóssága
+          </H3>
+          <Paragraph>
+            Az érme időjárásálló szálcsiszolt fém hatású felülettel rendelkező dekor műgyantából készül lézergravírozás segítségével, így hosszú távon is megőrzi minőségét, akár szélsőséges időjárási körülmények között is. Felragasztható beton sírkőre, márvány sírkőre, gránit sírkőre, vagy mészkő sírkőre. Természetesen az érmét egyéb helyre is ragaszthatod, melynek a felülete erre megfelelő.
+          </Paragraph>
+          
+          <Paragraph>
+            A QR-kódos érme ezután az érintésmentes technológiának köszönhetően bárki számára elérhetővé válik a szeretteidről szóló tartalom. Mindössze egy okostelefon szükséges ahhoz, hogy a QR-kódot beolvasva megnyíljon az emlékoldal, ahol a látogatók elmerülhetnek a megosztott emlékekben.
+          </Paragraph>
+        </div>
+
+        <div className="relative lex flex-col gap-4 bg-red-200 border border-[--error] p-8 rounded-3xl">
+            <CgDanger className="absolute -top-5 left-8 text-[--error] min-w-10 min-h-10 bg-neutral-50 rounded-full"/>
+            <Paragraph>
+                Az érme elhelyezése előtt a felületet tisztítuk meg a portól lehetőleg nedves törlőkendővel, és várjuk meg amíg megszárad. A tiszta felületen képes a kétoldalú ragasztó a legerősebb lenni.
+            </Paragraph>
+        </div>
+
+        <div className="relative flex flex-col gap-4 w-full bg-[--cream] p-8 rounded-3xl">
+          <H3 classname={"text-[--rose]"}>
+            Miért válaszd az EmlékQR érmét?
+          </H3>
+          <Paragraph>
+            Az emlékérme segítségével méltó módon emlékezhetsz meg szeretteidről. Legyen szó mindenszentek napjáról, egy különleges alkalomról, vagy mindennapi emlékezésről, az érme biztosítja, hogy az elhunytak emléke tovább éljen.
+          </Paragraph>
+        </div>
+
+        <Link
+            href={"/gyik"}
+            className="mt-4 self-center px-4 py-2 bg-[--blue] text-white rounded-full hover:bg-[--blue-hover] transition"
+          >
+            További tudnivalók
+          </Link>
 
       </div>
+
+    </div>
     </div>
   </>
   )
